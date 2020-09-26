199a7896f9afc5f19f5386c3e882919fcb7db0be
Yashvardhan Kukreja <yash.kukreja.98@gmail.com>
2020-09-26 19:24:24 +0530

    Genesis

---
+++ src/utils/fun_facts.js
@@ -0,0 +1,21 @@
+/**
+ * Schema/Format of the fact should be:
+ * {
+ *    "fact": "sample fact",
+ *    "quote": "author, if exists. Else feel free to leave this blank too :P"
+ * }
+ */
+module.exports.fun_facts = [
+    {
+        "fact": "There are a finite combinations of IP addresses possible. If we denote each possible IP address with a grain of sand, then the ultimate possible combinations of IPv4 addresses would be able to fill a truck, whille the ultimate possible combinations of IPv6 addresses would be able to fill the entire sun!",
+        "author": ""
+    },
+    {
+        "fact": "The first programmer in the world was a woman with the name Ada Lovelace",
+        "author": ""
+    },
+    {
+        "fact": "Computers understand nothing but 1's and 0's. The code write gets compiled and translated into those 1's and 0's so that the computer can understand it.",
+        "author": ""
+    }
+]
