f94934f00c37b4f7b5ee075c79fd5e5348fc4b5a
Alexander Craggs <github@femto.host>
2020-10-01 00:10:05 +0100

    add colour support

--- bin/index.js
+++ bin/index.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 const {fun_facts} = require("../src/utils/fun_facts");
+const colours = require("../src/utils/colours");
 
 idx = Math.floor(Math.random()*fun_facts.length);
 
-console.log(`"${fun_facts[idx]["fact"]}" ${fun_facts[idx]["author"] != "" ? "-"+fun_facts[idx]["author"] : "" }`);
+console.log(`${colours.fgYellow}"${fun_facts[idx]["fact"]}" ${fun_facts[idx]["author"] != "" ? "-"+fun_facts[idx]["author"] : "" }${colours.reset}`);
