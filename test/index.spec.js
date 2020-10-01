a8386d94fdcf4f5fa704e15e8922ad0dc3816c4a
Luis E. González <luisegr149@gmail.com>
2020-09-30 19:54:40 -0500

    Avoid random number for testing

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,10 +1,16 @@
 const app = require('../src/index');
-const {fun_facts} = require('../src/utils/fun_facts');
+let {fun_facts} = require('../src/utils/fun_facts');
 
 
-describe(`when runnig once`, () => {
-    it(`should log the fun fact with the author if there is one`, () => {
+/**
+ * checking for the random generated number, 
+ * this is not just Math.random(), 
+ * insted Math.floor(Math.random()*fun_facts.length);
+ */
+describe(`if the random generated number is 0`, () => {
+    it(`should show the first fact`, () => {
         console.log = jest.fn(); // mock console.log
+        Math.random = jest.fn(x => 0.123); // mock Math.random
         app(); // running app
         
         let printedFact = console.log.mock.calls[0][0].replace(/\"/g, '');
@@ -14,6 +20,7 @@ describe(`when runnig once`, () => {
             return printedFact == formedFact;
         });
 
-        expect(index).toBeGreaterThan(-1);
-    })
+
+        expect(index).toBe(0);
+    });
 })
